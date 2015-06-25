c05ac94fa74a567fb09340b2be487d14ea0889ac
francoise serrault <francoise.serrault@gmail.com>
2015-06-25 17:39:09 +0200

    facebook-connect

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,8 @@ Rails.application.routes.draw do
   resources :apartments
   resources :reservations
 
-  devise_for :users
+  devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
+
   resources :users, :only => [:show]
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
